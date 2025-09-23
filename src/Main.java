52f0f03b1190a4204756d964db806523864576d9
BruceLitvin <brucel15@nycstudents.net>
2025-09-23 18:08:34 -0400

    Finished book

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+    public static void main(String[] args) {
+        boolean[][] schedule = new boolean[8][60];
+        AppointmentBook a = new AppointmentBook(schedule);
+        for (int i = 10; i < 15; i++) schedule[1][i] = true;
+        for (int i = 25; i < 30; i++) schedule[1][i] = true;
+        for (int i = 0; i < 15; i++) schedule[2][i] = true;
+        for (int i = 41; i < 60; i++) schedule[2][i] = true;
+        for (int i = 5; i < 30; i++) schedule[3][i] = true;
+        for (int i = 44; i < 60; i++) schedule[3][i] = true;
+        try {
+            a.printPeriod(2);
+        } catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("You inserted a bad number you noob");
+        }
+        AppointmentBook b = new AppointmentBook(schedule);
+        int period=2;
+        while(period<5)
+        {
+            System.out.println("Period:"+ period);
+            b.printPeriod(period);
+            period++;
+        }
+        b.printPeriod(4);
+        System.out.println(b.makeAppointment(2,4,22));
+        b.printPeriod(4);
+    }
+
+}
