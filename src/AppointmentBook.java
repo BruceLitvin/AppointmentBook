650b4dbbf54dad8d35ff4d53dbe78774a8fbaa4f
BruceLitvin <brucel15@nycstudents.net>
2025-10-22 12:14:40 -0400

    E

--- src/AppointmentBook.java
+++ src/AppointmentBook.java
@@ -1,3 +1,4 @@
+
 public class AppointmentBook {
     private boolean[][] schedule;
 
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.io.FileNotFoundException;
+import java.io.File;
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
